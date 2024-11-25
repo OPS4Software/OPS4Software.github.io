203ef085cc718b4c560f58ac184a36686986f400
OPS4Software <management@inops.net>
2024-11-26 01:47:31 +0400

    test

---
+++ app.js
@@ -0,0 +1,17 @@
+let tg = window.Telegram.WebApp;
+
+document.getElementById('first_name').value = tg.initDataUnsafe.first_name;
+
+let trx_id = document.getElementById("trx_id")
+let description = document.getElementById("description")
+let attached_file = document.getElementById("attached_file")
+
+let submit = document.getElementById("submit")
+
+submit.addEventListener("click", () => {
+
+})
+
+
+
+
